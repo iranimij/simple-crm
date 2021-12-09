7ffa8cbdbabcab2ba70e53bbf91dbff41e34324f
iranimij1 <iman@artbees.net>
2021-12-09 18:58:23 +0330

    Adding readme.md

--- includes/templates/form-template.php
+++ includes/templates/form-template.php
@@ -11,7 +11,7 @@ $attributes = get_query_var( 'simple_crm_form_attributes' );
         </div>
         <div class="simple-crm-row">
             <label for="phone"><?php echo esc_html( $attributes['phone_label'] ) ?></label>
-            <input type="number" required id="phone" name="data[phone]" maxlength="<?php echo esc_attr( $attributes['phone_length'] ) ?>">
+            <input type="number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" required id="phone" name="data[phone]" maxlength="<?php echo esc_attr( $attributes['phone_length'] ) ?>">
         </div>
         <div class="simple-crm-row">
             <label for="email"><?php echo esc_html( $attributes['email_label'] ) ?></label>
@@ -19,14 +19,14 @@ $attributes = get_query_var( 'simple_crm_form_attributes' );
         </div>
         <div class="simple-crm-row">
             <label for="desire_budget"><?php echo esc_html( $attributes['desire_budget_label'] ) ?></label>
-            <input type="number" required id="desire_budget" name="data[desire_budget]" maxlength="<?php echo esc_attr( $attributes['desire_budget_length'] ) ?>">
+            <input type="number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" required id="desire_budget" name="data[desire_budget]" maxlength="<?php echo esc_attr( $attributes['desire_budget_length'] ) ?>">
         </div>
         <div class="simple-crm-row">
             <label for="message"><?php echo esc_html( $attributes['message_label'] ) ?></label>
-            <textarea name="data[message]" id="message" cols="<?php echo esc_attr( $attributes['message_columns'] ) ?>" rows="<?php echo esc_attr( $attributes['message_rows'] ) ?>"></textarea>
+            <textarea name="data[message]" id="message" cols="<?php echo esc_attr( $attributes['message_columns'] ) ?>" rows="<?php echo esc_attr( $attributes['message_rows'] ) ?>" maxlength="<?php echo esc_attr( $attributes['message_length'] ) ?>"></textarea>
         </div>
         <input type="hidden" name="data[current_time]" value="<?php echo esc_html( $attributes['current_time'] ) ?>">
-        <div class="simple-crm-row ">
+        <div class="simple-crm-row">
             <button type="submit" class="simple-crm-main-form-submit-button">Submit</button>
         </div>
         <div class="simple-crm-main-form-result">
--- readme.md
+++ readme.md
@@ -4,7 +4,19 @@ Create a simple CRM for wordpress,
 Using this shortcode :
 
 `
-[simple_crm_form name_label="name label" phone_label="Phone label" email_label="Email Label" desire_budget_label="Desire label" message_label="message Label" name_length="10" message_rows="3"]
+[
+    simple_crm_form name_label="name label"
+    phone_label="Phone label"
+    email_label="Email Label"
+    desire_budget_label="Desire label"
+    message_label="message Label"
+    name_length="10"
+    phone_length="20"
+    email_length="20"
+    desire_budget_length="20"
+    message_length="20"
+    message_rows="4"
+]
 `
 
 **== Installation ==**
